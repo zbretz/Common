d706fbd252086122a5dd2751a817cd14e5b6a435
NMarti20 <natasha.martinezsj@gmail.com>
2022-01-17 17:59:24 -0800

    added recruiter files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import RecruiterMain from "./Recruiter/RecruiterMain.jsx";
 
 const App = () => {
   return (
-  <div>
-    <h1>CommonApp</h1>
-  </div>
-  )
-}
+    <div>
+      <h1>CommonApp</h1>
+      <RecruiterMain />
+    </div>
+  );
+};
 
 export default App;
---
+++ client/components/Recruiter/RecruiterMain.jsx
@@ -0,0 +1,10 @@
+import "./RecruiterMain.css";
+import React from "react";
+
+const RecruiterMain = () => {
+  return <div>
+      <h1>Recruiter</h1>
+  </div>;
+};
+
+export default RecruiterMain;
